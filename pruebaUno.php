4659265dcf1b871dbea21fc55e7ab79f2905a051
gabriel <gabriel>
2016-03-24 17:33:38 -0600

    cambio de archivo php

---
+++ pruebaUno.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    
+<head>
+        
+<meta charset="UTF-8">
+        
+<title>
+</title>
+    
+</head>
+    
+<body>
+        
+<h1>My first PHP page</h1>
+   
+     
+<?php
+        
+echo "Hello World!";
+        
+?>
+    
+
+</body>
+
+</html>
